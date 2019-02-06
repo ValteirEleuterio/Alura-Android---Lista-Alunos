d7da1fdd2dc24c357cd3194b274be81d21d5ee94
ValteirEleuterio <Valteir.ea@hotmail.com>
2019-02-06 13:25:21 -0200

    dao

--- app/src/main/java/alura/com/br/FormularioActivity.java
+++ app/src/main/java/alura/com/br/FormularioActivity.java
@@ -8,12 +8,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import alura.com.br.dao.AlunoDAO;
+import alura.com.br.modelo.Aluno;
+
 public class FormularioActivity extends AppCompatActivity {
+    private FormularioHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        this.helper = new FormularioHelper(this);
     }
 
     @Override
@@ -26,7 +32,13 @@ public class FormularioActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.menu_formulario_ok:
-                Toast.makeText(FormularioActivity.this, "Aluno salvo com sucesso!", Toast.LENGTH_SHORT).show();
+                Aluno aluno = helper.pegaAluno();
+
+                AlunoDAO dao = new AlunoDAO(this);
+                dao.insere(aluno);
+                dao.close();
+
+                Toast.makeText(this, "Aluno " + aluno.getNome() + " salvo! ", Toast.LENGTH_SHORT).show();
                 finish();
                 break;
         }
